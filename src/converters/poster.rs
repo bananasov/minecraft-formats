10ba7f5a24ee53c7b37db02bfa67ed2dad209020
sovnya <enp7s1@tutanota.com>
2024-03-13 13:59:19 +0100

    I blame rust analyzer.

--- src/converters/poster.rs
+++ src/converters/poster.rs
@@ -3,10 +3,10 @@ use crate::formats::poster::{Poster, Posters};
 impl From<Poster> for Posters {
     fn from(value: Poster) -> Self {
         Self {
-            title: value.label,
+            title: value.clone().label, // Its silly but like i dont know a better way, lmao
             width: Some(1),
             height: Some(1),
-            pages: vec![value.clone()], // Its silly but like i dont know a better way, lmao
+            pages: vec![value],
         }
     }
 }
